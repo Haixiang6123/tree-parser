fa268aa8c70c41c502b16e49a35c97aaadaaa161
Haixiang6123 <haixiang6123@gmail.com>
2020-10-22 00:28:06 +0800

    feat: 添加少量注释

--- src/toTable.js
+++ src/toTable.js
@@ -1,12 +1,17 @@
+// Helper 函数
 const dfsHelper = (object, names, step, tempRow, result) => {
+  // 异常值的情况
   if (!object) return;
 
+  // 如果超过 names 长度，则开始搜集结果
   if (step === names.length) {
     return result.push({ ...tempRow });
   }
 
+  // 获取对应的 key 的名字
   const keyName = names[step];
 
+  // 如果值为数组，则直接 push 数组里的每个元素
   if (object instanceof Array) {
     return object.map(item => {
       result.push({
@@ -19,8 +24,10 @@ const dfsHelper = (object, names, step, tempRow, result) => {
   const [key] = Object.keys(object);
   const values = Object.values(object);
 
+  // 复制一份临时的对象
   tempRow = { ...tempRow, [keyName]: key };
 
+  // 继续下一层的递归
   values.forEach((value => {
     dfsHelper(value, names, step + 1, tempRow, result);
   }))
